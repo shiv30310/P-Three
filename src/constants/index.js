5dc5f3bef16a057f323d18499f356806cf2d52ca
Itachi Uchiha <shiv30310@gmail.com>
2023-07-11 09:23:06 +0530

    update

--- src/constants/index.js
+++ src/constants/index.js
@@ -121,39 +121,41 @@ const experiences = [
     iconBg: "#383E56",
     date: "June 2021- Present",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
-      "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
-      "Implementing responsive design and ensuring cross-browser compatibility.",
-      "Participating in code reviews and providing constructive feedback to other developers.",
+      'Amano ONE - Cloud-based Parking Access and Revenue Control Systemt.',
+      'Developed responsive UI components for AmanoONE web application using React.',
+      'Translated wireframes into pixel-perfect interfaces, ensuring a seamless user experience.',
+      'Improved performance and maintainability through efficient and reusable code.',
+      'Collaborated with backend team for seamless integration and data synchronization.',
+      'Updated knowledge through self-learning and industry conferences.',
     ],
   },
 ];
 
 const testimonials = [
-//   {
-//     testimonial:
-//       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-//     name: "Sara Lee",
-//     designation: "CFO",
-//     company: "Acme Co",
-//     image: "https://randomuser.me/api/portraits/women/4.jpg",
-//   },
-//   {
-//     testimonial:
-//       "I've never met a web developer who truly cares about their clients' success like Rick does.",
-//     name: "Chris Brown",
-//     designation: "COO",
-//     company: "DEF Corp",
-//     image: "https://randomuser.me/api/portraits/men/5.jpg",
-//   },
-//   {
-//     testimonial:
-//       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-//     name: "Lisa Wang",
-//     designation: "CTO",
-//     company: "456 Enterprises",
-//     image: "https://randomuser.me/api/portraits/women/6.jpg",
-//   },
+  //   {
+  //     testimonial:
+  //       "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
+  //     name: "Sara Lee",
+  //     designation: "CFO",
+  //     company: "Acme Co",
+  //     image: "https://randomuser.me/api/portraits/women/4.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "I've never met a web developer who truly cares about their clients' success like Rick does.",
+  //     name: "Chris Brown",
+  //     designation: "COO",
+  //     company: "DEF Corp",
+  //     image: "https://randomuser.me/api/portraits/men/5.jpg",
+  //   },
+  //   {
+  //     testimonial:
+  //       "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
+  //     name: "Lisa Wang",
+  //     designation: "CTO",
+  //     company: "456 Enterprises",
+  //     image: "https://randomuser.me/api/portraits/women/6.jpg",
+  //   },
 ];
 
 const projects = [
@@ -222,4 +224,4 @@ const projects = [
   // },
 ];
 
-export { services, technologies, experiences, testimonials ,projects };
+export { services, technologies, experiences, testimonials, projects };
